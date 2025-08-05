ba3dd56eedb5dfec9c848da62697e8517af4973c
Er-Dharmendra-kewat <techaboutneed0@gmail.com>
2025-08-05 21:57:57 +0530

    for of and for in loop

---
+++ JS-Basics/for_of_loop.js
@@ -0,0 +1,29 @@
+const names = ["Youtube","facebook","Instagram","Netflix","Amazon"];
+
+// for(const n of names){ for of loop is used with array and strings and it returns values
+//     console.log(n);
+    
+// }
+
+// const social = {
+//     yt: "Youtube",
+//     ig: "Instagram",
+//     fb: "Facebook",
+//     Ico: "LearnCodeOnline.in",
+// };
+
+// for (const n in social) { // for in loop is used with objects which returns the keys/values/index
+//     // console.log(`${social[n]}`);
+//     console.log(`key is: ${n} and value is: ${social[n]}`);
+    
+   
+// }
+
+
+let fruits  = ["apple","banana","mango"];
+
+for (const key in fruits) {
+    console.log(`${fruits[key]}`);
+    
+}
+
--- JS-Basics/functions/p1.js
+++ JS-Basics/functions/p1.js
@@ -1,5 +1,9 @@
-//  console.log("abc".toUpperCase())
+// splice() in JavaScript is a powerful method used to:
+// Remove items from an array
+// Insert new items into an array
+// Or do both at the same time!
 
- a=[1,3,4,5]
- a.splice(3);
- console.log(a)
+
+a = [1, 3, 4, 5]
+a.splice(3);
+console.log(a)
